0ee1b5b370a43741a4aff937935e821d4fcd81a5
sreepadma <sreepadmalab21@gmail.com>
2022-05-05 20:39:39 +0530

    adding cloneplusoperator

--- stockreport.py
+++ stockreport.py
@@ -33,7 +33,7 @@ with col1:
 with col2:
  clone = st.multiselect("Clone", options=['SR1','AS201H','CM1A','LK1','GE1A','HP81A','CR61A','ET41','CR41A','CR102A','EP1','ET1','CR81A','EP21','CR121E','LB1','CR22C','LR1','SR41','RR1','MC21','MU1','AS151K','BC41 Z','BC1Z','RN1', 'CR1','CR21','CR121A','CR21','CR21A','CR103','CR103A','CR141','CR161','CR181','CR201','CR221','CR221A','CR241','CR261','CR241A','CR181A',
 'CR21C','HP1','HP21','HP41','HP81','SR22','SGJ','AR1A','AG1', 'AG21','AGSRC','MC1','MS41','MS61','MS81','MS101','ASIE','AS1F','AS51F','AS51G','AS51I','AS51J','AS151A','AS151I','AS201A','AS201F','AS251D','AS251E','AS251F','AS251G','AS251H','AS251I','AS251K','AS401','AS401A','CR281','AS151L','CR241C','CR241D','CR261C','AS201J','BC101A','AS101D','AS51L','CR201D','CR201C','AS451A','RC1','AS101E','AS1I',
-'AS301','AS301E','AS301F','AS301I','AS301J','AS301K','AS301L','AS301M','AS301N','AS301O','AS351','AS351A','AS351C','BC61','BC1','BC21','BC41','CR121C','CR121D','CF1','AG21A','AG21C','GE1','AR1','CR22','CR22A','BC1 Z','BC41 Z', 'CR121','CR41','CR61','CR81','CR102','CR141A','BC81','BC101','CR261A','CM1','AS451','CR201A','CR201C','AS151J','AS101D','AS51M','AS201K','AS1K','AS1L','AS51N'])
+'AS301','AS301E','AS301F','AS301I','AS301J','AS301K','AS301L','AS301M','AS301N','AS301O','AS351','AS351A','AS351C','BC61','BC1','BC21','BC41','CR121C','CR121D','CF1','AG21A','AG21C','GE1','AR1','CR22','CR22A','BC1 Z','BC41 Z', 'CR121','CR41','CR61','CR81','CR102','CR141A','BC81','BC101','CR261A','CM1','AS451','CR201A','CR201C','AS151J','AS101D','AS51M','AS201K','AS1K','AS1L','AS51N','BC101D','AS351D','AS1J','RF1'])
  clone=str(clone)[2:-2]
 col1,col2,col3,col4=st.columns(4)
 with col1:
@@ -53,7 +53,7 @@ with col2:
  produced_stage=st.multiselect("Produced stage ",options= ("M","M2","S","R","INI","MA","MN","MB"))
  produced_stage = str(produced_stage)[2:-2]
 with col3:
- operator=st.multiselect("Operator",options= ("NJK","SGS","ANJ","SKM","OMR","SST","ATH",'SPS'))
+ operator=st.multiselect("Operator",options= ("NJK","SGS","ANJ","SKM","OMR","SST","ATH","SPS","SUM"))
  operator = str(operator)[2:-2]
 with col4:
  produced_status=st.text_input("Produced status")
